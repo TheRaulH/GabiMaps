c92975a8f0067d64c812adf6ca56545ca9065a5d
TheRaulH <raulherreracruz18@gmail.com>
2025-04-17 22:35:31 -0400

    refactor: remove location-related models and repository implementations

--- lib/domain/repositories/location_repository.dart
+++
@@ -1,9 +0,0 @@
-import '../entities/location.dart';
-
-abstract class LocationRepository {
-  Future<List<LocationEntity>> getNearbyLocations(
-    double latitude,
-    double longitude,
-  );
-  Future<void> saveLocation(LocationEntity location);
-}
