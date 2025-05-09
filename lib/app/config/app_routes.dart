ce91947ff9dcaeb0ad46895b258e172b54159cda
TheRaulH <raulherreracruz18@gmail.com>
2025-05-09 18:22:37 -0400

    fix: update import paths and clean up route definitions in app_routes.dart

--- lib/app/config/app_routes.dart
+++ lib/app/config/app_routes.dart
@@ -1,19 +1,20 @@
-// lib/presentation/screenstree/routes.dart
+// lib/app/config/routes.dart
 import 'package:flutter/material.dart';
+import 'package:gabimaps/features/auth/ui/login_screen.dart';
+import 'package:gabimaps/features/auth/ui/reset_password_screen.dart';
 import 'package:gabimaps/features/home/ui/red_social.dart';
 import 'package:gabimaps/features/home/ui/saved.dart';
+import 'package:gabimaps/features/user/ui/profile_screen.dart';
 import 'package:gabimaps/layout/main_app.dart';
 import 'package:gabimaps/features/map/ui/map_screen.dart';
 import 'package:gabimaps/features/notifications/ui/notifications_screen.dart';
 import 'package:gabimaps/features/map/ui/locations_management_page.dart';
-import 'package:gabimaps/features/user/ui/user_management_page.dart';
-import '../../features/auth/ui/login_screen.dart'; // Ruta de importación correcta
-import '../../features/auth/ui/register_screen.dart'; // Ruta de importación correcta
-import '../../features/home/ui/home_screen.dart'; // Ruta de importación correcta
-import '../../features/map/ui/location_details_screen.dart'; // Ruta de importación correcta
-import '../../features/user/ui/profile_screen.dart'; // Ruta de importación correcta
-import '../../layout/splash_screen.dart'; // Ruta de importación correcta
-import '../../features/user/ui/settings_page.dart'; // Ruta de importación correcta
+import 'package:gabimaps/features/user/ui/user_management_page.dart'; 
+import '../../features/auth/ui/register_screen.dart';
+import '../../features/map/ui/location_details_screen.dart';
+import '../../layout/splash_screen.dart';
+import '../../features/user/ui/settings_page.dart';  
+
 
 class AppRoutes {
   static const String splash = '/splash';
@@ -22,54 +23,57 @@ class AppRoutes {
   static const String home = '/home';
   static const String locationDetails = '/locationDetails';
   static const String profile = '/profile';
-  static const String map = '/map'; // Ruta para el mapa
+  static const String map = '/map'; 
   static const String notifications =
-      '/notifications'; // Ruta para las notificaciones
-  static const String mainapp = '/MainApp'; // Ruta para la pantalla de error
-  static const String settings = '/settings'; // Ruta para la configuración
+      '/notifications';
+  static const String mainapp = '/MainApp';
+  static const String settings = '/settings';  
   static const String location =
-      '/admin/locations'; // Ruta para el perfil de usuario
+      '/admin/locations'; 
   static const String users =
-      '/admin/users'; // Ruta para la gestión de usuarios (si es necesario)
+      '/admin/users'; 
 
   static const String redsocial =
-      '/redsocial'; // Ruta para la red social (si es necesario)
+      '/redsocial'; 
 
   static const String saved =
-      '/saved'; // Ruta para la pantalla de guardados (si es necesario)
+      '/saved';
+
+  static const String resetPassword = '/resetPassword';
 
 
   static Map<String, WidgetBuilder> routes = {
     splash: (context) => SplashScreen(),
     login: (context) => LoginScreen(),
-    register: (context) => RegisterScreen(),
-    home: (context) => HomeScreen(),
+    register: (context) => RegisterScreen(), 
     locationDetails: (context) => LocationDetailsScreen(),
-    profile: (context) => UserProfilePage(),
-    map: (context) => MapScreen(), // Asegúrate de que este widget esté definido
+    profile: (context) => ProfileScreen(),
+    map: (context) => MapScreen(), 
     notifications:
         (context) =>
-            NotificationsScreen(), // Asegúrate de que este widget esté definido
+            NotificationsScreen(), 
     mainapp:
-        (context) => MainApp(), // Asegúrate de que este widget esté definido
+        (context) => MainApp(),
 
     settings:
         (context) =>
-            SettingsPage(), // Asegúrate de que este widget esté definido
+            SettingsPage(),
 
     location:
         (context) =>
-            LocationsManagementPage(), // Asegúrate de que este widget esté definido
+            LocationsManagementPage(),
     users:
         (context) =>
-            UserManagementPage(), // Asegúrate de que este widget esté definido
+            UserManagementPage(),
 
     redsocial:
         (context) =>
-            UAGRMRedSocialApp(), // Asegúrate de que este widget esté definido
+            UAGRMRedSocialApp(),
 
     saved:
         (context) =>
-            GuardadosPage(), // Asegúrate de que este widget esté definido
+            GuardadosPage(),
+
+    resetPassword: (context) => ResetPasswordScreen(), 
   };
 }
