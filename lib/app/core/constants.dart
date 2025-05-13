a675f52d7243d8bf97e96319824efccf9ea13c52
TheRaulH <raulherreracruz18@gmail.com>
2025-05-13 15:52:18 -0400

    feat: add location categories to AppConstants

---
+++ lib/app/core/constants.dart
@@ -0,0 +1,14 @@
+class AppConstants {
+  static const List<String> locationCategories = [
+    'Módulo',
+    'Curichi (punto de encuentro)',
+    'Biblioteca',
+    'Tienda',
+    'Baño',
+    'Centro Interno',
+    'Librería',
+    'Dirección de Carrera',
+    'CPD',
+    'Estacionamiento',
+  ];
+}
